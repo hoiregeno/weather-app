141f768cb0235a1fd133234efe0f8a4154144c01
hoiregeno <hoiregeno75@gmail.com>
2025-06-11 11:41:09 +1000

    first commit

---
+++ src/components/WeatherApp.jsx
@@ -0,0 +1,105 @@
+import React, { useRef, useState, useEffect } from "react";
+
+function WeatherApp() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const inputRef = useRef(null);
+  const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+  // Auto-focus to the input field on startup.
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, []);
+
+  // Handles the city entered.
+  const handleCityInput = async (e) => {
+    e.preventDefault(); // Prevent default form behaviour
+    const trimmed = city.trim(); // Grab the city enterd and trim any unnecessary whitespaces.
+
+    // If there's no city entered, Exit handleCityInput function
+    if (!trimmed) return;
+
+    // Quit if already fetching
+    if (isLoading) return;
+
+    setIsLoading(true); // Start the loading spinner
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${trimmed}&appid=${apiKey}&units=metric`;
+      const response = await fetch(url); // Fetch weather data
+      if (!response.ok) {
+        throw new Error(`Could not locate "${city}". Please try again.`);
+      }
+
+      // Parse the response into object
+      const data = await response.json();
+      setWeather(data); // Store the data object into weather
+      setError(""); // Reset any previous errors
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+      setWeather(null);
+    } finally {
+      setCity(""); // Clear input field before/after entering city.
+      setIsLoading(false); // Stop the loading spinner after fetch is success/fail.
+    }
+  };
+
+  return (
+    <>
+      <h1 className="app-title">Weather App</h1>
+
+      <form className="weather-form" onSubmit={handleCityInput}>
+        <input
+          type="text"
+          placeholder="Enter city"
+          value={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+            setError("");
+          }}
+          ref={inputRef}
+          required
+        />
+        <button
+          type="submit"
+          disabled={isLoading}
+          style={
+            isLoading
+              ? { cursor: "not-allowed", backgroundColor: "gray" }
+              : { cursor: "pointer" }
+          }
+          aria-label="search city"
+        >
+          Search
+        </button>
+      </form>
+
+      {error && <p className="error-message">{error}</p>}
+      {isLoading && (
+        <div className="loading-wrapper">
+          <div className="loading-wrapper__spinner"></div>
+        </div>
+      )}
+
+      {weather && !isLoading && (
+        <div className="card">
+          <h1 className="card__city">
+            {weather.name}, {weather.sys.country}
+          </h1>
+          <p className="card__temp">{weather.main.temp.toFixed(2)}°C</p>
+          <p className="card__humidity">humidity: {weather.main.humidity}%</p>
+          <p className="card__description">{weather.weather[0].description}</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+            className="card__weather-icon"
+          />
+        </div>
+      )}
+    </>
+  );
+}
+
+export default WeatherApp;
